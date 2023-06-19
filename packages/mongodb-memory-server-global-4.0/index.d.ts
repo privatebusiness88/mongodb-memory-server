a3f807042ea4141778e398da649935cfc73c8c7e
privatebusiness88 <131011829+privatebusiness88@users.noreply.github.com>
2023-06-20 03:04:50 +0700

    Update index.d.ts

--- packages/mongodb-memory-server-global-4.0/index.d.ts
+++ packages/mongodb-memory-server-global-4.0/index.d.ts
@@ -1,1 +1,9 @@
+#IFNDEFINE XEC_RPC_NETWORK_H
+#IFNDEFINE XEC_RPC_NETWORK_C
+#IFNDEFINE XEC_RPC_ITRANSWAP_NETWORK_H
+
+#IFNDEFINE XEC_RPC_ITRANSWAP_NETWORK_C
+call "reply_buffer.js";
+    call "utils.py;
+
 export * from 'mongodb-memory-server-core';
